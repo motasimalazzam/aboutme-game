ee7b2d351a4019ee2d2c34fb42e16ee5c8b76731
motasimalazzam <motasim.h.alazzam@gmail.com>
2021-03-31 04:37:56 +0300

    addin 2 questins

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 ## aboutme-game
 
-I built a website with a quiz game. The content of the questions is information about me and the user answers them with yes or no.
+I built a website with a quiz game. The content of the questions is information about me and the user answers them with yes or no. Add two guessing questions, one is guessing a number between 1 and 10, and the other is guessing at least one player out of 4 players.
 
 ### Author: Mo'tasim Al-Azzam
 
@@ -10,12 +10,12 @@ I built a website with a quiz game. The content of the questions is information 
 
 * How did this go, overall?
 
-* At first, I felt that the task was complicated, but after reading what was required of the assignment and developing a plan, the task became less complicated, and it was good training in dealing with writing different codes.
+* In the beginning it seemed easy, but upon implementation, difficulties arose, especially in creating the last question. I faced a problem in calculating the final result and I performed all the possibilities that I know, and finally I solved the problem.
 
 * What observations or questions do you have about what you’ve learned so far?
 
-* I have a question, is it possible to add a file like .eslintrc.json file or change its name in VS but not in the terminal?
+* I have no question about this lab.
 
 * How long did it take you to complete this assignment? And, before you started, how long did you think it would take you to complete this assignment?
 
-* The task took about two and a half hours, but at first I thought I needed a full day.
+* Frankly I thought it wouldn't take more than two hours but it took more than 6 hours to solve a problem in calculating the score in the last question.
--- js/app.js
+++ js/app.js
@@ -1,71 +1,125 @@
 'use strict';
-let UserName=prompt("what is your name");
-alert("welcome "+ UserName);
-alert("Now let's play, Yes-No questions will appear on your screen, and you can answer with yes/y or no/ n.");
+let score=0;
+let userName=prompt('what is your name');
+while (!userName) {
+  alert('pleas write your name');
+  userName=prompt('what is your name');
+}
+alert('welcome '+ userName);
+alert('Now lets play, Yes-No questions will appear on your screen, and you can answer with yes/y or no/ n.');
 
 // first question
-let LoveMyMajor=prompt("Do you think I love my college major?");
-LoveMyMajor=LoveMyMajor.toLowerCase();
+let loveMyMajor=prompt('Do you think I love my college major?');
+loveMyMajor=loveMyMajor.toLowerCase();
 //console.log(LoveMyMajor);
-if (LoveMyMajor==='yes' || LoveMyMajor==='y') {
-
-    alert("Certainly, I love my major. My goal during my high school education was entering the college of Mechanical engineering!");
-}else if(LoveMyMajor==='no' || LoveMyMajor==='n'){
-    alert("Sorry it is wrong answer. I love my major very much.");
+if (loveMyMajor==='yes' || loveMyMajor==='y') {
+  alert('Certainly, I love my major. My goal during my high school education was entering the college of Mechanical engineering!');
+  score++;
+  console.log('first',score);
+}else if(loveMyMajor==='no' || loveMyMajor==='n'){
+  alert('Sorry it is wrong answer. I love my major very much.');
 }else{
-    alert("Please this is yes-no questions so you can ONLY answer yes or no!!!");
+  alert('Please this is yes-no questions so you can ONLY answer yes or no!!!');
 }
 
 // second question
-let FavoriteClub=prompt("Do you think Real Madrid is my favorite club?");
-FavoriteClub=FavoriteClub.toLowerCase();
+let favoriteClub=prompt('Do you think Real Madrid is my favorite club?');
+favoriteClub=favoriteClub.toLowerCase();
 //console.log(FavoriteClub);
-if (FavoriteClub==='yes' || FavoriteClub==='y') {
-
-    alert("Of course yes, in fact it is the strongest and best club in history !!");
-}else if(FavoriteClub==='no' || FavoriteClub==='n'){
-    alert("That makes me a little annoyed. I will not support a club less powerful and great than Real Madrid, so, sorry it is wrong answer.");
+if (favoriteClub==='yes' || favoriteClub==='y') {
+  alert('Of course yes, in fact it is the strongest and best club in history !!');
+  score++;
+  console.log('second',score);
+}else if(favoriteClub==='no' || favoriteClub==='n'){
+  alert('That makes me a little annoyed. I will not support a club less powerful and great than Real Madrid, so, sorry it is wrong answer.');
 }else{
-    alert("Please this is yes-no questions so you can ONLY answer yes or no!!!");
+  alert('Please this is yes-no questions so you can ONLY answer yes or no!!!');
 }
-
 // third question
-let LoveProgramming=prompt("Do you think I hate programming?");
-LoveProgramming=LoveProgramming.toLowerCase();
+let loveProgramming=prompt('Do you think I hate programming?');
+loveProgramming=loveProgramming.toLowerCase();
 //console.log(LoveProgramming);
-if (LoveProgramming==='yes' || LoveProgramming==='y') {
-
-    alert("Are you serious? If I did not love programming I would not have joined this scholarship");
-}else if(LoveProgramming==='no' || LoveProgramming==='n'){
-    alert("I only expected this answer. It is a correct answer. I love programming");
+if (loveProgramming==='yes' || loveProgramming==='y') {
+  alert('Are you serious? If I did not love programming I would not have joined this scholarship');
+}else if(loveProgramming==='no' || loveProgramming==='n'){
+  alert('I only expected this answer. It is a correct answer. I love programming');
+  score++;
+  console.log('third',score);
 }else{
-    alert("Please this is yes-no questions so you can ONLY answer yes or no!!!");
+  alert('Please this is yes-no questions so you can ONLY answer yes or no!!!');
 }
-
 // fourth question
-let PassScholarship=prompt("Do you think I can successfully pass this scholarship?");
-PassScholarship=PassScholarship.toLowerCase();
+let passScholarship=prompt('Do you think I can successfully pass this scholarship?');
+passScholarship=passScholarship.toLowerCase();
 //console.log(PassScholarship);
-if (PassScholarship==='yes' || PassScholarship==='y') {
+if (passScholarship==='yes' || passScholarship==='y') {
 
-    alert("Thanks for your trust in me, I will do my best for this scholarship.");
-}else if(PassScholarship==='no' || PassScholarship==='n'){
-    alert("Well, I am not sad, and I will do my best to pass it successfully.");
+  alert('Thanks for your trust in me, I will do my best for this scholarship.');
+  score++;
+  console.log('fourth',score);
+}else if(passScholarship==='no' || passScholarship==='n'){
+  alert('Well, I am not sad, and I will do my best to pass it successfully.');
 }else{
-    alert("Please this is yes-no questions so you can ONLY answer yes or no!!!");
+  alert('Please this is yes-no questions so you can ONLY answer yes or no!!!');
 }
 
 //fifth question
-let MyTall=prompt("Do you think I am tall?");
-MyTall=MyTall.toLowerCase();
+let myTall=prompt('Do you think I am tall?');
+myTall=myTall.toLowerCase();
 //console.log(MyTall);
-if (MyTall==='yes' || MyTall==='y') {
+if (myTall==='yes' || myTall==='y') {
 
-    alert("Yes, I am considered tall, almost my tall is 183 cm.");
-}else if(MyTall==='no' || MyTall==='n'){
-    alert("Well, if we consider the length of 183 cm is short, your answer is correct");
+  alert('Yes, I am considered tall, almost my tall is 183 cm.');
+  score++;
+  console.log('fIfth',score);
+}else if(myTall==='no' || myTall==='n'){
+  alert('Well, if we consider the length of 183 cm is short, your answer is correct');
 }else{
-    alert("Please this is yes-no questions so you can ONLY answer yes or no!!!");
+  alert('Please this is yes-no questions so you can ONLY answer yes or no!!!');
 }
 
-alert('Thank you '+UserName+'. I hope you enjoyed this game, if you want to know more about me read the paragraph on the page, goodbye.');
+//  sixth question
+let favoriteNumber=prompt('Guess what is my favorite number between 1 and 10');
+favoriteNumber=parseInt(favoriteNumber);
+for(let i=0;i<4;i++){
+  if (favoriteNumber === 7){
+    score++;
+    console.log('sixth',score);
+    break;
+  }else if(favoriteNumber > 7){
+    console.log(favoriteNumber ,'>7');
+    favoriteNumber=prompt('too high, try agin');
+    favoriteNumber=parseInt(favoriteNumber);
+  }else if(favoriteNumber < 7){
+    console.log( favoriteNumber ,'<7');
+    favoriteNumber=prompt('too low, try agin');
+    favoriteNumber=parseInt(favoriteNumber);
+  }
+
+}
+alert('the correct answer is 7');
+//seventh question
+let trials=0;
+let correct=true;
+let favoritePlayer=['ronaldo','Ronaldinho','Marcelo','Zidan'];
+do {
+  let theplayer=prompt('Who is my favorite player');
+  for(let j=0; j<4; j++){
+    if(theplayer===favoritePlayer[j]){
+      score++;
+      alert('correct answer');
+      correct=false;
+      j=6;
+    }
+  }
+  trials++;
+  if(!correct){
+    trials=7;
+  }
+  if(trials===6 ){
+    alert('out of attempts');
+  }
+}while(trials<=5);
+alert(favoritePlayer+' are all corect');
+alert('Thank you '+userName+'. I hope you enjoyed this game, if you want to know more about me read the paragraph on the page, goodbye.');
+alert('your score: '+score+' out of 7');
